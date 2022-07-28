63bafced624b5874ea56e62dee1f685137c97df9
hmzsumon <zsumonn@gmail.com>
2022-07-28 10:51:18 +0600

    firsta

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    const response = await axios.get('/products');
+    setData(response.data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1> Front-End Success Auto Deploy CI/CD </h1>
+        <h4> This is a EC2 Test Ci </h4>
+        <div className='product-wrapper'>
+          {data.map((product) => (
+            <div className='product' key={product.id}>
+              <h3>{product.name}</h3>
+              <p>{product.price}</p>
+            </div>
+          ))}
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
